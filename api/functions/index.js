524c2bb80931ec474c68c7ef4fd4dd293fbceb88
Ha10 <horiiortho5@gmail.com>
2017-11-09 14:25:04 +0900

    initialize app as firebase functions app

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
 exports.kqctimes = functions.https.onRequest((request, response) => {
   switch (request.method) {
