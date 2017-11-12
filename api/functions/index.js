6ef932ba82f81e6a4d99a845601f754dd2fc2a8e
Yasunori Horii <horiiortho5@gmail.com>
2017-11-12 13:09:38 +0900

    implement post information method

--- api/functions/index.js
+++ api/functions/index.js
@@ -16,15 +16,15 @@ exports.kqctimes = functions.https.onRequest((request, response) => {
   }
 })
 
-function postKqctimes(request, response) {
+function postKqctimes (request, response) {
   if (checkKqctimes(request) === false) {
-    response.status(400).send({ message : 'Bad Request'})
+    response.status(400).send({ message: 'Bad Request' })
   } else {
-    response.status(201).send({ message : 'KQCTimes created'})
+    response.status(201).send({ message: 'KQCTimes created' })
   }
 }
 
-function checkKqctimes(request) {
+function checkKqctimes (request) {
   const title = request.body.title
   const subTitle = request.body.sub_title
   const publisher = request.body.publisher
@@ -42,9 +42,9 @@ function checkKqctimes(request) {
   }
 }
 
-function getKqctimes(request, response) {
-  // return values of Kqctimes. The number of Kqctimes return is 10
-  response.status(200).send({ message : 'GET request' })
+function getKqctimes (request, response) {
+    // return values of Kqctimes. The number of Kqctimes return is 10
+  response.status(200).send({ message: 'GET request' })
 }
 
 exports.information = functions.https.onRequest((request, response) => {
@@ -61,20 +61,37 @@ exports.information = functions.https.onRequest((request, response) => {
   }
 })
 
-function getInformation(request, response) {
-  // return values of Information. The number of Information return is 10
-  response.status(200).send({ message : 'GET request' })
+function getInformation (request, response) {
+    // return values of Information. The number of Information return is 10
+  response.status(200).send({ message: 'GET request' })
 }
 
-function postInformation(request, response) {
+function postInformation (request, response) {
   if (checkInformation(request) === false) {
-    response.status(400).send({ message : 'Bad Request'})
+    response.status(400).send({ message: 'Bad Request' })
   } else {
-    response.status(201).send({ message : 'Information created'})
+    const title = request.body.title
+    const publisher = request.body.publisher
+    const body = request.body.body
+
+    let jsonStr = {
+      'id': genInformationId(),
+      'title': title,
+      'publisher': publisher,
+      'body': body
+    }
+    const jsonObject = JSON.parse(jsonStr)
+    admin.database().ref('/development/information')
+      .push(jsonObject).then(snapshot => {
+        response.status(201).send({ message: 'Information created' })
+      })
+      .catch(error => {
+        response.status(418).send(error)
+      })
   }
 }
 
-function checkInformation(request) {
+function checkInformation (request) {
   const title = request.body.title
   const publisher = request.body.publisher
   const body = request.body.body
@@ -86,20 +103,48 @@ function checkInformation(request) {
   }
 }
 
+function genInformationId () {
+  let id = getUniqueId()
+  return id + 'info'
+}
+
+/*
+function genKqctimesId () {
+  let id = getUniqueId
+  return id + 'kqct'
+}
+*/
+
+// generate UniqueId for information and Kqctimes
+function getUniqueId () {
+  // 使用文字の定義
+  const str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+  // 桁数の定義
+  var len = 16
+
+  // ランダムな文字列の生成
+  var result = ''
+  for (var i = 0; i < len; i++) {
+    result += str.charAt(Math.floor(Math.random() * str.length))
+  }
+  return result
+}
+
 exports.login = functions.https.onRequest((request, response) => {
-  // In this endpoint, cheack user name and password, and return 200 Success or
-  // 404 Not Found. In this application, the kind of users KQC-Web use is three.
-  //
-  // Users
-  // 1. Common KQC members.
-  //   this user can use only for the purpose of visiting contents of Kqctimes or
-  //   Informations. They can not create, delete the Kqctimes, but they can create
-  //   or delete Information. Because in private page (I call it MEMBERS PAGE),
-  //   all contents except KQCtimes is public for all members
-  // 2. Manager
-  //   this user can not only visiting contents KQC-Web has, also create or delete
-  //   contents contains KQCtimes. This user has no authority to change system
-  //   program, but can order developpers to create or fix this application
-  // 3. Developper
-  //    this user has authority to develop this application
+    // In this endpoint, cheack user name and password, and return 200 Success or
+    // 404 Not Found. In this application, the kind of users KQC-Web use is three.
+    //
+    // Users
+    // 1. Common KQC members.
+    //   this user can use only for the purpose of visiting contents of Kqctimes or
+    //   Informations. They can not create, delete the Kqctimes, but they can create
+    //   or delete Information. Because in private page (I call it MEMBERS PAGE),
+    //   all contents except KQCtimes is public for all members
+    // 2. Manager
+    //   this user can not only visiting contents KQC-Web has, also create or delete
+    //   contents contains KQCtimes. This user has no authority to change system
+    //   program, but can order developpers to create or fix this application
+    // 3. Developper
+    //    this user has authority to develop this application
 })
